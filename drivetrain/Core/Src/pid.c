4c32432296d2498caea11cec0e26c51b85571b8e
Ryan Lee <rlee663818@gmail.com>
2024-01-28 18:53:51 -0800

    Implemented PID for a single value

---
+++ drivetrain/Core/Src/pid.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+
+typedef struct pid_state_struct {
+	uint16_t previous_error;
+	uint16_t sum_error;
+} pid_history;
+
+
+
+uint8_t update_pid(pid_state* pid_state, uint16_t speed_expected, uint16_t speed_actual) {
+	uint16_t ctrl_err = speed_expected - speed_actual;
+	uint16_t err_delta = ctrl_err - previous_error;
+	pid_state->previous_error = ctrl_err;
+	pid_state->sum_error += ctrl_err;
+	return P_GAIN * ctrl_err + D_GAIN * err_delta + I_GAIN * pid_state->sum_error;
+}
+
+void update_pid_arr(pid_state* pid_states, ) {
+
+}
