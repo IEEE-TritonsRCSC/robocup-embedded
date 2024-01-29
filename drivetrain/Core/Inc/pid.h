0b1629ce378ed63ffae731cc768c8a27dd17fd63
Ryan Lee <rlee663818@gmail.com>
2024-01-28 19:04:03 -0800

    added arbitrary gain constants to make code compile

--- drivetrain/Core/Inc/pid.h
+++ drivetrain/Core/Inc/pid.h
@@ -8,6 +8,12 @@
 #ifndef INC_PID_H_
 #define INC_PID_H_
 
+// TODO: Update with better values
+#define PID_P_GAIN = 0.2;
+#define PID_D_GAIN = 0.2;
+#define PID_I_GAIN = 0.2;
+
+
 uint16_t update_pid(pid_state* pid_state, uint16_t speed_expected, uint16_t speed_actual);
 
 #endif /* INC_PID_H_ */
