f14ce8f1649a9fbd688c223e399090daca08eb90
danielbonkowsky <danielbonkowsky@gmail.com>
2025-01-13 15:30:51 -0800

    Added code so recieving properly aligns packets

--- stm_testing/recieving/recieving.ino
+++ stm_testing/recieving/recieving.ino
@@ -1,8 +1,11 @@
 // Use Serial2 for demonstration
 HardwareSerial leSerial(2);
 
-#define TX 16 // TX=GPIO17
-#define RX 17 // RX=GPIO16
+#define TX 17 // TX=GPIO17
+#define RX 16 // RX=GPIO16
+
+#define HEADER_BYTE_1 0xCA
+#define HEADER_BYTE_2 0xFE
 
 void setup() {
     // Initialize serial ports
@@ -13,14 +16,19 @@ void setup() {
 }
 
 void loop() {
-    if (leSerial.available() >= 11) {  // Check if at least 11 bytes are available
-        uint8_t byteArray[11];        // Create an array to store the data
-        leSerial.readBytes(byteArray, 11); // Read 11 bytes into the array
 
-        // Print the received data for debugging
-        for (int i = 0; i < 11; i++) {
+    // Read from UART2 -- properly aligns messages with 0xCA 0xFE header
+    if (leSerial.available() >= 11) {
+      if (leSerial.read() == HEADER_BYTE_1 && leSerial.read() == HEADER_BYTE_2) {
+          uint8_t byteArray[9];
+          leSerial.readBytes(byteArray, 9);
+
+          // Print the received data for debugging
+          Serial.printf("0x%02X 0x%02X ", HEADER_BYTE_1, HEADER_BYTE_2);
+          for (int i = 0; i < 9; i++) {
             Serial.printf("0x%02X ", byteArray[i]); // Print as hex
-        }
-        Serial.println();
+          }
+          Serial.println();
+      }
     }
 }
