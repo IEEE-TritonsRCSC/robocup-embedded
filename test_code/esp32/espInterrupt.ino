3ce9009887fa0ed238d1153e67515813f5dbea07
Andrew <155210303+SUNW4VE@users.noreply.github.com>
2025-01-31 17:46:33 -0800

    Update espInterrupt.ino

--- test_code/esp32/espInterrupt.ino
+++ test_code/esp32/espInterrupt.ino
@@ -1,23 +1,91 @@
-#define INTERRUPT_PIN 16
+// Define pins
+#define TX 17
+#define RX 16
 
-volatile bool interrupt_flag = false;
+// UART setup
+HardwareSerial espSerial(2);
 
-void IRAM_ATTR handleInterrupt() {
-  interrupt_flag = true;
-}
+volatile bool received_interrupt = false;
+int transmitCount = 0;
+int receiveCount = 0;
+std::array<unsigned char, 11> msg = {0xca, 0xfe, 
+                                     0x00, 0x64, 0x00, 0x64, 
+                                     0x00, 0x64, 0x00, 0x64, 
+                                     0x11};
+
+void IRAM_ATTR handleInterrupt();
+inline void readReceivedData();
+inline void readSentData();
 
 void setup() {
+
   Serial.begin(115200);
-  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN),
-                                        handleInterrupt,
-                                        FALLING);
+  espSerial.begin(115200, SERIAL_8N1, RX, TX);
+
+  pinMode(16, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(RX),
+                  handleInterrupt,
+                  FALLING);
 }
 
 void loop() {
-  if (interrupt_flag) {
-    Serial.println("Interrupt detected!");
-    interrupt_flag = false;
+
+  // receive
+  if (received_interrupt) {
+    readReceivedData();
+    received_interrupt = false;
   }
+  
+  // send
+  espSerial.write(msg.data(), msg.size());
+  readSentData();
+
+
+  delay(2000);
 
 }
+
+void IRAM_ATTR handleInterrupt() {
+  received_interrupt = true;
+}
+
+inline void readReceivedData() {
+
+  Serial.printf("RECEIVE (%d) ", receiveCount++);
+  
+  unsigned char receivedMsg[11] = {0};  // Buffer for received data
+
+  // Wait until we have at least 11 bytes in the buffer
+  uint32_t startTime = millis();
+  while (espSerial.available() < 11) {
+    if (millis() - startTime > 100) { // Timeout after 100ms
+      Serial.println("Timeout waiting for data.");
+      return;
+    }
+  }
+
+  // Read exactly 11 bytes
+  espSerial.readBytes(receivedMsg, 11);
+
+  // Print received data as hex
+  for (int i = 0; i < 11; i++) {
+    Serial.printf("%02X ", receivedMsg[i]);
+  }
+  
+  Serial.println();
+}
+
+inline void readSentData() {
+
+  Serial.printf("TRANSMIT (%d) ", transmitCount++);
+  for (int i = 0; i < 11; i++) {
+      Serial.printf("%02X ", msg.data()[i]); // Print as uppercase hex with leading zeroes
+  }
+
+  Serial.println();
+}
+
+
+
+
+
