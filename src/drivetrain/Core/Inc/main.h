0205e5720a0967063276ff670b00ca549e543615
danielbonkowsky <danielbonkowsky@gmail.com>
2025-02-05 09:48:36 -0800

    Formatting and added led on/off defines

--- src/drivetrain/Core/Inc/main.h
+++ src/drivetrain/Core/Inc/main.h
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
@@ -57,7 +57,10 @@ void backward(int motorSpeed, int duration);
 void left(int motorSpeed, int duration);
 void right(int motorSpeed, int duration);
 void setMotorSpeeds(int16_t ms1, int16_t ms2, int16_t ms3, int16_t ms4);
-void runMotors(unsigned char motorOneHigh, unsigned char motorOneLow, unsigned char motorTwoHigh, unsigned char motorTwoLow, unsigned char motorThreeHigh, unsigned char motorThreeLow, unsigned char motorFourHigh, unsigned char motorFourLow);
+void runMotors(unsigned char motorOneHigh, unsigned char motorOneLow,
+		unsigned char motorTwoHigh, unsigned char motorTwoLow,
+		unsigned char motorThreeHigh, unsigned char motorThreeLow,
+		unsigned char motorFourHigh, unsigned char motorFourLow);
 void kick(int kickDuration);
 void Error_Handler(void);
 
@@ -74,6 +77,8 @@ void noDribble();
 #define LED_GREEN_PORT GPIOF
 #define LED_RED_PIN GPIO_PIN_11
 #define LED_RED_PORT GPIOE
+#define LED_ON GPIO_PIN_RESET
+#define LED_OFF GPIO_PIN_SET
 
 /*
  * motor 1 = front right motor
