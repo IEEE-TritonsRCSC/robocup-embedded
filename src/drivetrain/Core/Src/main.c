77293fb7f6469dd5ab08178bffd5bd27bcf7cbc2
danielbonkowsky <danielbonkowsky@gmail.com>
2025-02-05 09:48:56 -0800

    Moved PID back to main loop and formatting

--- src/drivetrain/Core/Src/main.c
+++ src/drivetrain/Core/Src/main.c
@@ -1,22 +1,24 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 #include "main.h"
 #include "can.h"
 #include "dma.h"
@@ -24,10 +26,7 @@
 #include "usart.h"
 #include "gpio.h"
 #include "PID.h"
-// #include "pid.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include <string.h>
 /* USER CODE END Includes */
 
@@ -38,28 +37,30 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define UART_RX_BUFFER_SIZE 9   // set to the size we want to limit receive messages to
+#define UART_TX_BUFFER_SIZE 12  // set to the size we want to limit send messages to
+#define HEADER_BYTE_1 0xCA
+#define HEADER_BYTE_2 0xFE
+#define KICK 0x14
+#define REDUCTION_RATIO 36.0
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-#define UART_RX_BUFFER_SIZE 11  //set to the size we want to limit receive messages to
-#define UART_TX_BUFFER_SIZE 12  //set to the size we want to limit send messages to
-#define RUN_HEADER 0xCAFE
-#define KICK 0x14
-#define REDUCTION_RATIO 36.0
+
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-
 /* USER CODE BEGIN PV */
-//CAN variables
+
+// CAN variables
 CAN_TxHeaderTypeDef canTxHeader;
 CAN_RxHeaderTypeDef canRxHeader;
 uint32_t canTxMailbox;
 uint8_t CAN_TxData[8];
 uint8_t CAN_RxData[8];
 
-//PID feedback variables
+// PID feedback variables
 volatile uint8_t motor_idx;
 volatile uint16_t angle_data[4];
 volatile float speed_data[4];
@@ -80,18 +81,17 @@ volatile float Kp4 = 1;
 volatile float Ki4 = 0.4;
 volatile float Kd4 = 0;
 
-//UART setup
-uint8_t uart_rx_buffer[UART_RX_BUFFER_SIZE]; //buffer that stores in an array of characters user inputs, aka a string
+// UART setup
+uint8_t uart_rx_buffer[UART_RX_BUFFER_SIZE]; // buffer that stores in an array of characters user inputs, aka a string
 uint8_t uart_tx_buffer[UART_TX_BUFFER_SIZE];
 
-uint8_t msg_buff[9];
-volatile uint8_t rx_byte;
-volatile int ca_flag;
-volatile int fe_flag;
+volatile uint8_t rx_byte = 0;
+volatile int header1_flag = 0;
+volatile int header2_flag = 0;
 
-volatile int timeout;  //timeout for safety mechanism to shutoff robot
+volatile int timeout;  // timeout for safety mechanism to shutoff robot
 
-volatile int kickFlag; //flag for kicking
+volatile int kickFlag; // flag for kicking
 
 /* USER CODE END PV */
 
@@ -107,9 +107,9 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 
 int main(void) {
 	/* USER CODE BEGIN 1 */
@@ -139,8 +139,6 @@ int main(void) {
 	MX_USART2_UART_Init();
 	/* USER CODE BEGIN 2 */
 
-	//HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-
 	// Motor setup
 	HAL_GPIO_TogglePin(MOTOR_PORT, MOTOR1_PIN);
 	HAL_GPIO_TogglePin(MOTOR_PORT, MOTOR2_PIN);
@@ -155,52 +153,46 @@ int main(void) {
 	canTxHeader.TransmitGlobalTime = DISABLE;
 
 	// PID Setup
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; ++i) {
 		speed_data[i] = 0;
 	}
-	pid_init(&motor_pid[0],9999,1000,20,0,Kp1,Ki1,Kd1);
-	pid_init(&motor_pid[1],9999,1000,20,0,Kp2,Ki2,Kd2);
-	pid_init(&motor_pid[2],9999,1000,20,0,Kp3,Ki3,Kd3);
-	pid_init(&motor_pid[3],9999,1000,20,0,Kp4,Ki4,Kd4);
+	pid_init(&motor_pid[0], 9999, 1000, 20, 0, Kp1, Ki1, Kd1);
+	pid_init(&motor_pid[1], 9999, 1000, 20, 0, Kp2, Ki2, Kd2);
+	pid_init(&motor_pid[2], 9999, 1000, 20, 0, Kp3, Ki3, Kd3);
+	pid_init(&motor_pid[3], 9999, 1000, 20, 0, Kp4, Ki4, Kd4);
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; ++i) {
 		targetSpeeds[i] = 0;
 	}
 
-	// HAL_UART_Receive_IT(&huart2, uart_rx_buffer, UART_RX_BUFFER_SIZE);
+	// Start program with LEDs off
+	HAL_GPIO_WritePin(LED_GREEN_PORT, LED_GREEN_PIN, LED_OFF);
+	HAL_GPIO_WritePin(LED_RED_PORT, LED_RED_PIN, LED_OFF);
 
 	HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
-
 
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
-
-	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_14, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, GPIO_PIN_SET);
-
 	while (1) {
-		if (kickFlag == 1) {               //Triggers a kick
+		if (kickFlag == 1) {               // Triggers a kick
 			kick(20);
 			kickFlag = 0;
 		}
 
-		if (timeout >= 500) {                 //Safety timeout when UART disconnects
-			for (int i = 0; i < 4; i++) {
+		if (timeout >= 500) {             // Safety timeout when UART disconnects
+			for (int i = 0; i < 4; ++i) {
 				targetSpeeds[i] = 0;
 			}
 		}
 
-		/*
-		for (int i = 0; i < 4; i++) {                          //PID control loop
-			motor_pid[i].target = targetSpeeds[i];
-			pid_calculate(&motor_pid[i],speed_data[i]);
-		}
+		 for (int i = 0; i < 4; ++i) {                          // PID control loop
+			 motor_pid[i].target = targetSpeeds[i];
+			 pid_calculate(&motor_pid[i],speed_data[i]);
+		 }
 
-		setMotorSpeeds((motor_pid[0].output), (motor_pid[1].output), (motor_pid[2].output), (motor_pid[3].output));
-		*/
-
+		 setMotorSpeeds((motor_pid[0].output), (motor_pid[1].output), (motor_pid[2].output), (motor_pid[3].output));
 
 
 //	  int feedbackSize = sizeof(speed_data);
@@ -220,27 +212,32 @@ int main(void) {
 //	  HAL_UART_Transmit_DMA(&huart2, feedbackBuffer, sizeof(feedbackBuffer));
 		timeout++;
 		HAL_Delay(10);
-   /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-   /* USER CODE BEGIN 3 */
- /* USER CODE END 3 */
+		/* USER CODE BEGIN 3 */
+		/* USER CODE END 3 */
 	}
 }
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
 	//HAL_GPIO_TogglePin(LED_GREEN_PORT,LED_GREEN_PIN);
-    if(hcan == &hcan1) {
-    	HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &canRxHeader, CAN_RxData);
+	if (hcan == &hcan1) {
+		HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &canRxHeader, CAN_RxData);
 
-        if(canRxHeader.StdId == 0x201) motor_idx = 0;
-        if(canRxHeader.StdId == 0x202) motor_idx = 1;
-        if(canRxHeader.StdId == 0x203) motor_idx = 2;
-        if(canRxHeader.StdId == 0x204) motor_idx = 3;
+		if (canRxHeader.StdId == 0x201)
+			motor_idx = 0;
+		if (canRxHeader.StdId == 0x202)
+			motor_idx = 1;
+		if (canRxHeader.StdId == 0x203)
+			motor_idx = 2;
+		if (canRxHeader.StdId == 0x204)
+			motor_idx = 3;
 
-        angle_data[motor_idx] = (uint16_t)(CAN_RxData[0]<<8 | CAN_RxData[1]);
-        speed_data[motor_idx] = ((int16_t)(CAN_RxData[2]<<8 | CAN_RxData[3]) / REDUCTION_RATIO) ; // angular velocity in rpm
-        torque_current_data[motor_idx] = (CAN_RxData[4]<<8 | CAN_RxData[5]);
-    }
+		angle_data[motor_idx] = (uint16_t) (CAN_RxData[0] << 8 | CAN_RxData[1]);
+		speed_data[motor_idx] = ((int16_t) (CAN_RxData[2] << 8 | CAN_RxData[3])
+				/ REDUCTION_RATIO); // angular velocity in rpm
+		torque_current_data[motor_idx] = (CAN_RxData[4] << 8 | CAN_RxData[5]);
+	}
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
@@ -248,140 +245,104 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 	 * Called when uart_rx_buffer is full
 	 */
 
-	if (!ca_flag) {
+	if (!header1_flag) {
+		if (rx_byte == HEADER_BYTE_1) {
+			// first header byte received
 
-		if (rx_byte == 0xCA) {
-			ca_flag = 1;
+			header1_flag = 1;
 			HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
 			return;
 		}
-
 	} else {
 
-		if (fe_flag) {
+		if (header2_flag) {
+			// full message received
 
 			timeout = 0;
 
-			targetSpeeds[0] = (int16_t)((msg_buff[0] << 8) | msg_buff[1]);
-			targetSpeeds[1] = (int16_t)((msg_buff[2] << 8) | msg_buff[3]);
-			targetSpeeds[2] = (int16_t)((msg_buff[4] << 8) | msg_buff[5]);
-			targetSpeeds[3] = (int16_t)((msg_buff[6] << 8) | msg_buff[7]);
+			targetSpeeds[0] = (int16_t)((uart_rx_buffer[0] << 8) | uart_rx_buffer[1]);
+			targetSpeeds[1] = (int16_t)((uart_rx_buffer[2] << 8) | uart_rx_buffer[3]);
+			targetSpeeds[2] = (int16_t)((uart_rx_buffer[4] << 8) | uart_rx_buffer[5]);
+			targetSpeeds[3] = (int16_t)((uart_rx_buffer[6] << 8) | uart_rx_buffer[7]);
 
+			/*
+			 * Previously |targetSpeeds[i]| <= 500
+			 */
 
-			ca_flag = fe_flag = 0;
-
-			for (int i = 0; i < 4; i++) {                          //PID control loop
-				motor_pid[i].target = targetSpeeds[i];
-				pid_calculate(&motor_pid[i], speed_data[i]);
+			if (uart_rx_buffer[9] == KICK) {
+				 kickFlag = 1;
 			}
 
-			setMotorSpeeds((motor_pid[0].output), (motor_pid[1].output), (motor_pid[2].output), (motor_pid[3].output));
+			for (int i = 0; i < UART_RX_BUFFER_SIZE; ++i) {
+				uart_rx_buffer[i] = 0;
+			}
 
-			// setMotorSpeeds(targetSpeeds[0], targetSpeeds[1], targetSpeeds[2], targetSpeeds[3]);
+			header1_flag = header2_flag = 0;
 
 			HAL_GPIO_TogglePin(LED_GREEN_PORT, LED_GREEN_PIN);
 			HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
-
 			return;
 
-		} else if (rx_byte == 0xFE) {
-			fe_flag = 1;
-			HAL_UART_Receive_IT(&huart2, msg_buff, 9);
+		} else if (rx_byte == HEADER_BYTE_2) {
+			// second header byte received
+
+			header2_flag = 1;
+			HAL_UART_Receive_IT(&huart2, uart_rx_buffer, UART_RX_BUFFER_SIZE);
 			return;
 		} else {
-			ca_flag = 0;
+			// first header byte received but not followed by second header byte
+
+			header1_flag = 0;
 			HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
 			return;
 		}
-
-
 	}
-
-	/*
-	if (((uart_rx_buffer[0] << 8) | (uart_rx_buffer[1])) == RUN_HEADER) {
-
-		timeout = 0;
-
-		targetSpeeds[0] = (int16_t)((uart_rx_buffer[2] << 8) | uart_rx_buffer[3]);
-		targetSpeeds[1] = (int16_t)((uart_rx_buffer[4] << 8) | uart_rx_buffer[5]);
-		targetSpeeds[2] = (int16_t)((uart_rx_buffer[6] << 8) | uart_rx_buffer[7]);
-		targetSpeeds[3] = (int16_t)((uart_rx_buffer[8] << 8) | uart_rx_buffer[9]);
-
-		for (int i = 0; i < 4; i++) {
-			if (targetSpeeds[i] > 500) {
-				targetSpeeds[i] = 500;
-			} else if (targetSpeeds[i] < -500) {
-				targetSpeeds[i] = -500;
-			}
-		}
-
-		if (uart_rx_buffer[10] == KICK) {
-			kickFlag = 1;
-		}
-
-
-		for (int i = 0; i < 11; i++) {
-			uart_rx_buffer[i] = 0;
-		}
-
-		HAL_GPIO_TogglePin(LED_RED_PORT, LED_RED_PIN);
-
-	}
-
-
-	HAL_GPIO_TogglePin(LED_GREEN_PORT, LED_GREEN_PIN);
-	HAL_UART_Receive_IT(&huart2, uart_rx_buffer, UART_RX_BUFFER_SIZE);
-	*/
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 6;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 6;
+	RCC_OscInitStruct.PLL.PLLN = 168;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
-void setMotorSpeeds(int16_t ms1, int16_t ms2, int16_t ms3, int16_t ms4)
-{
+void setMotorSpeeds(int16_t ms1, int16_t ms2, int16_t ms3, int16_t ms4) {
 	uint8_t h1 = ms1 >> 8;
 	uint8_t l1 = ms1;
 	uint8_t h2 = ms2 >> 8;
@@ -393,12 +354,12 @@ void setMotorSpeeds(int16_t ms1, int16_t ms2, int16_t ms3, int16_t ms4)
 	runMotors(h1, l1, h2, l2, h3, l3, h4, l4);
 }
 
-void runMotors(unsigned char motorOneHigh, unsigned char motorOneLow, unsigned char motorTwoHigh, 
-unsigned char motorTwoLow, unsigned char motorThreeHigh, unsigned char motorThreeLow, unsigned char motorFourHigh, 
-unsigned char motorFourLow)
-{          
-  //speed can be 16 bits, split into high and low bytes
-	CAN_TxData[0] = motorOneHigh;  //high byte for speed, shifted 8 because only buffer is only 8 bits
+void runMotors(unsigned char motorOneHigh, unsigned char motorOneLow,
+		unsigned char motorTwoHigh, unsigned char motorTwoLow,
+		unsigned char motorThreeHigh, unsigned char motorThreeLow,
+		unsigned char motorFourHigh, unsigned char motorFourLow) {
+	//speed can be 16 bits, split into high and low bytes
+	CAN_TxData[0] = motorOneHigh; //high byte for speed, shifted 8 because only buffer is only 8 bits
 	CAN_TxData[1] = motorOneLow;       //low bytes for speed
 	CAN_TxData[2] = motorTwoHigh;
 	CAN_TxData[3] = motorTwoLow;
@@ -409,8 +370,7 @@ unsigned char motorFourLow)
 	HAL_CAN_AddTxMessage(&hcan1, &canTxHeader, CAN_TxData, &canTxMailbox);
 }
 
-void kick(int kickDuration)
-{
+void kick(int kickDuration) {
 	HAL_GPIO_WritePin(KICKER_PORT, KICKER_PIN, GPIO_PIN_SET);
 	HAL_Delay(kickDuration);
 	HAL_GPIO_WritePin(KICKER_PORT, KICKER_PIN, GPIO_PIN_RESET);
@@ -419,41 +379,36 @@ void kick(int kickDuration)
 /* USER CODE END 4 */
 
 /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM6 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM6 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	/* USER CODE BEGIN Callback 0 */
 
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM6) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
 
-{
-  /* USER CODE BEGIN Callback 0 */
-
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM6) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
-
-  /* USER CODE END Callback 1 */
+	/* USER CODE END Callback 1 */
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
