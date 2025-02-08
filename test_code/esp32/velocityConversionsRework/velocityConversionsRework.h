5ea7a470c292e5cf15bd1483003cffef06ff443d
Andrew <155210303+SUNW4VE@users.noreply.github.com>
2025-02-07 17:12:01 -0800

    Update velocityConversionsRework.h

--- test_code/esp32/velocityConversionsRework/velocityConversionsRework.h
+++ test_code/esp32/velocityConversionsRework/velocityConversionsRework.h
@@ -17,22 +17,20 @@ constexpr double ANGLE_TL = M_PI/3;
 constexpr double MS_TO_RPM = r / 60;
 
 // wheel angles RELATIVE TO THE X AXIS
-double[] wheelAngles = {ANGLE_TR, ANGLE_BR, ANGLE_BL, ANGLE_TL} // 1234
+std::array<double, 4> wheelAngles = {ANGLE_TR, ANGLE_BR, ANGLE_BL, ANGLE_TL}; // 1234
 
 // position of each wheel relative to center
-double[] posX = { d * sin(M_PI / 6), d * (-sin(M_PI / 6)),   // 1, 2
+std::array<double, 4> posX = { d * sin(M_PI / 6), d * (-sin(M_PI / 6)),   // 1, 2
                          d * (-sin(M_PI / 6)), d * sin(M_PI / 6) };    // 3, 4
-double[] posY = { d * cos(M_PI / 6), d * cos(M_PI / 6),      // 1, 2
+std::array<double, 4> posY = { d * cos(M_PI / 6), d * cos(M_PI / 6),      // 1, 2
                          d * (-cos(M_PI / 6)), d * (-cos(M_PI / 6)) }; // 3, 4
 
 
-void getVelocityArray(array<int, 4>& wheel_speeds, double heading, double absV,
+void getVelocityArray(std::array<int, 4>& wheel_speeds, double heading, double absV,
                       double theta, double rotV);
 
-void valuesToBytes(array<int, 4>& wheel_speeds, array<uint8_t, 8>& wheel_speeds_byte);
+void valuesToBytes(std::array<int, 4>& wheel_speeds, std::array<uint8_t, 8>& wheel_speeds_byte);
 
-void getWheelVelocities(array<int, 4>& wheel_speeds, 
-                        proto_simulation_RobotMoveCommand& action);
+void getWheelVelocities(std::array<int, 4>& wheel_speeds, _RobotMoveCommand& action);
 
-void action_to_byte_array(array<uint8_t, 8>& wheel_speeds_byte,
-                          proto_simulation_RobotMoveCommand& action);
+void action_to_byte_array(std::array<uint8_t, 8>& wheel_speeds_byte, _RobotMoveCommand& action);
