90c06599b305e6eba51f73e795c126b3132b855c
danielbonkowsky <danielbonkowsky@gmail.com>
2025-02-09 22:25:59 -0800

    Modifications to test server

--- src/TestServer/TestProtobufServer.py
+++ src/TestServer/TestProtobufServer.py
@@ -23,10 +23,10 @@ message.vision.pixel_y = 1.0
 message.vision.height = 1.0
 
 message.command.id = 1
-message.command.move_command.local_velocity.forward = 10
-message.command.move_command.local_velocity.left = 0
+message.command.move_command.local_velocity.forward = 0
+message.command.move_command.local_velocity.left = 0 
 message.command.move_command.local_velocity.angular = 0
-message.command.kick_speed = 0
+message.command.kick_speed = 10
 message.command.kick_angle = 0
 message.command.dribbler_speed = 0
 
@@ -34,12 +34,11 @@ UDP_IP = '192.168.8.80' # Replace with the ESP32's IP address
 UDP_PORT = 3333
 
 MULTICAST_GROUP = '224.1.1.1'
-PORT = 10510
+PORT = 10500
 
 
 def main():
     # Create a UDP socket
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     ttl = struct.pack('b', 1)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
@@ -53,18 +52,23 @@ def main():
         while True:
             # Send data
             sock.sendto(data, (MULTICAST_GROUP, PORT))
-            print(f'Sent data, velocity = {message.command.move_command.local_velocity.forward}')
+
+            if (message.command.kick_speed > 0):
+                message.command.kick_speed = 0;
+
+            print(f'Sent data, velocity = {message.command.move_command.local_velocity.angular}')
 
             if (count % 5 == 0):
-                if (message.command.move_command.local_velocity.forward == 10):
-                    message.command.move_command.local_velocity.forward = 0    
+                if (message.command.move_command.local_velocity.angular != 0):
+                    message.command.move_command.local_velocity.angular = 0    
                     data = message.SerializeToString()
                 else:
-                    message.command.move_command.local_velocity.forward = 10    
-                    data = message.SerializeToString()
+                   message.command.move_command.local_velocity.angular = 12 
+                   message.command.kick_speed = 10;
+                   data = message.SerializeToString()
 
             count += 1 
-            time.sleep(2)
+            time.sleep(1)
 
        
     
