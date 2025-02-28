301b643f10092895f0efff2a93e6f0ecdf2151ef
danielbonkowsky <danielbonkowsky@gmail.com>
2025-02-28 15:27:01 -0800

    Modified TransmitUART to send fast and slow messages

--- test_code/esp32/TransmitUART/TransmitUART.ino
+++ test_code/esp32/TransmitUART/TransmitUART.ino
@@ -1,33 +1,51 @@
 // Define pins
 #define TX 17
 #define RX 16
+#define MESSAGE_LENGTH 11
 
 // UART setup
 HardwareSerial espSerial(2);
 
-int count;
+int count = 0;
+bool fast = true;
+uint8_t fast_msg[MESSAGE_LENGTH] = {0xca, 0xfe, 0x03, 0xe8, 0x03, 0xe8, 0x03, 0xe8, 0x03, 0xe8, 0x01};
+uint8_t slow_msg[MESSAGE_LENGTH] = {0xca, 0xfe, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0, 0x00, 0x00, 0x00};
 
 void setup() {
   Serial.begin(115200);
   espSerial.begin(115200, SERIAL_8N1, RX, TX);
-
-  count = 0;
 }
 
 void loop() {
-  std::array<unsigned char, 11> msg = {0xca, 0xfe, 0x00, 0x64, 0x00, 0x64, 0x00, 0x64, 0x00, 0x64, 0x11};
-  
-  espSerial.write(msg.data(), msg.size());
 
-  Serial.printf("(%d) ", count);
-  for (int i = 0; i < 11; i++) {
-      Serial.printf("%02X ", msg.data()[i]); // Print as uppercase hex with leading zeroes
-  }
-  Serial.println();
-
-  // espSerial.flush();
   count++;
+  Serial.printf("(%d) ", count);
 
-  delay(1000);
 
+  if (fast) {
+    espSerial.write(fast_msg, MESSAGE_LENGTH);
+
+    
+    for (int i = 0; i < 11; i++) {
+      Serial.printf("%02x ", fast_msg[i]); // Print as uppercase hex with leading zeroes
+    }
+
+    Serial.println();
+    
+    fast = false;
+    delay(1000);
+
+  } else {
+    espSerial.write(slow_msg, MESSAGE_LENGTH);
+
+    
+    for (int i = 0; i < 11; i++) {
+      Serial.printf("%02x ", slow_msg[i]); // Print as uppercase hex with leading zeroes
+    }
+    Serial.println();
+    
+    fast = true;
+    delay(1000);
+  }
+  
 }
