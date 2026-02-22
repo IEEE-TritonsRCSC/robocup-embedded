adccec12489e6461d4aa322d641790b8132d93a1
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-02-21 23:41:50 -0800

    refactor main logic into helper functions no build errors or warnings NOTE: not tested/flashed on robot

---
+++ src/drivetraincpp/Include/helpers.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "stdint.h"
+#include "PID_Data.h"
+
+void updateDribblerSpeedFromFlag(int dribble_flag, int16_t* dribble_speed);
+void applySafetyTimeoutToTargetSpeeds(int timeout, float *targetSpeeds);
+void updateMotorPidLoop(PID_Data *motor_pids, float *targetSpeeds, float *speed_data);
---
+++ src/drivetraincpp/Sources/helpers.cpp
@@ -0,0 +1,35 @@
+#include "helpers.h"
+
+#define DRIBBLE_SPEED 1500
+#define TIMEOUT_DELAY 200
+
+void updateDribblerSpeedFromFlag(int dribble_flag, int16_t* dribble_speed)
+{
+    if (dribble_flag)
+    { // Turns dribbling on/off
+        *dribble_speed = DRIBBLE_SPEED;
+    }
+    else
+    {
+        *dribble_speed = 0;
+    }
+}
+
+void applySafetyTimeoutToTargetSpeeds(int timeout, float *targetSpeeds)
+{
+    if (timeout >= TIMEOUT_DELAY)
+    { // Safety timeout when UART disconnects
+        for (int i = 0; i < 4; ++i)
+        {
+            targetSpeeds[i] = 0;
+        }
+    }
+}
+
+void updateMotorPidLoop(PID_Data* motor_pids, float* targetSpeeds, float* speed_data) {
+    for (int i = 0; i < 4; ++i)
+    { // PID control loop
+        motor_pids[i].setTarget(targetSpeeds[i]);
+        motor_pids[i].pidCalculate(speed_data[i]);
+    }
+}
