5ac1c5d59f7eb1f227bfcaff64422d6f01d974ce
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2026-03-08 18:47:54 -0700

    Resovle debug and .setting file merge conflictpython

--- src/esp32/tester.py
+++ src/esp32/tester.py
@@ -4,7 +4,10 @@ import socket
 
 ROBOT_ID = 1
 COMMAND_IP = "239.42.42.42"
-COMMAND_PORT = 11000
+COMMAND_PORT = 10000
+
+# Yellow team: 10000
+# Blue team: 11000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
