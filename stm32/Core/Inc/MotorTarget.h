7b3c91bfe49e2c83f710cbcf0bb8be9b619bb6d4
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-04-11 16:37:57 -0700

    update comment for MotorTarget

--- stm32/Core/Inc/MotorTarget.h
+++ stm32/Core/Inc/MotorTarget.h
@@ -5,8 +5,8 @@
 /**
  * @brief struct for 5 target velocities for each motor from an ESP32 command
  * 
- * @note `velocities` must have 5 elements because the motor target queue size
- * is 20 bytes (4 byte float * 5 motors = 20 bytes)
+ * @note `velocities` is 5 elements, so the motor target queue size is 20 bytes 
+ * (4 byte float * 5 motors = 20 bytes)
  */
 struct MotorTarget {
    float velocities[NUM_MOTORS];
