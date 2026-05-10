cddd3043c61caea7c41c4f4246c8e7977194062f
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-05-10 01:51:13 -0700

    working UDP parse example

---
+++ robocup/robocup/send.py
@@ -0,0 +1,11 @@
+import socket
+
+ARDUINO_IP = "100.105.19.66"
+UDP_PORT   = 4210
+MESSAGE    = "Hello Arduino!"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MESSAGE.encode(), (ARDUINO_IP, UDP_PORT))
+sock.close()
+
+print(f"Sent '{MESSAGE}' to {ARDUINO_IP}:{UDP_PORT}")
