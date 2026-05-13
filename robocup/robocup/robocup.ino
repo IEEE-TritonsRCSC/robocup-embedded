66b1825a1f0889fa334d6ae1b967363285e1da6a
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-05-12 18:32:25 -0700

    fix: robocup.ino to wait for linux to boot and wait for peripherals to connect before executing loop

--- robocup/robocup/robocup.ino
+++ robocup/robocup/robocup.ino
@@ -12,18 +12,35 @@
 #include "WiFiControl.h"
 #include "LEDControl.h"
 
-
+// BEGIN CONSTEXPR
+constexpr unsigned int LINUX_BOOT_TIME = 30000; // it takes the linux on the UNO Q 30 seconds to boot up
+// END CONSTEXPR
 
 BridgeUDP<> udp(Bridge);
 char packetBuffer[BUFFER_SIZE];
 
 void setup() {
-  initLEDoff();
-  Bridge.begin(); // start router bridge
-  Monitor.begin(); // start serial monitor
-  udp.begin(PORT); // start udp connection
+  delay(LINUX_BOOT_TIME);
+
+  initLEDon();
+
+  while (!Bridge.begin()) { // start router bridge
+    delay(1000);
+  }
+
+  while (!Monitor.begin()) { // start serial monitor
+    delay(1000);
+  }
+
+  Monitor.print("Connecting UDP...");
+  while (!udp.begin(PORT)) { // start udp connection
+    Monitor.print(".");
+    delay(1000);
+  }
+  Monitor.println("");
 
   Monitor.println("Ready");
+  LEDoff();
 }
 
 void loop() {
@@ -31,5 +48,4 @@ void loop() {
     parsePacket(udp, Monitor,packetBuffer); 
     printPacket(Monitor,packetBuffer);
   }
-  delay(1000);
 }
