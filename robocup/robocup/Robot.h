818a6fee333691f89a158be6fe7e15de26d6b67a
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-05-12 20:19:03 -0700

    add Robot.h and Command.h

---
+++ robocup/robocup/Robot.h
@@ -0,0 +1,11 @@
+/**
+ * Robot.h
+ * information about the robot
+ */
+
+// BEGIN CONSTEXPR
+constexpr unsigned int ROBOT_ID = 1;
+constexpr unsigned int NUM_WHEELS = 4;
+constexpr unsigned int NUM_MOTORS = 5;
+// END CONSTEXPR
+
