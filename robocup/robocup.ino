ddc1096f1afec373c4a3bace98f14df87ee87aaa
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-06-26 00:05:18 -0700

    feat: display Tritonbots logo bitmap during setup display animation note: UDP commands are not arriving or getting parsed by the esp32 for some reason

--- robocup/robocup.ino
+++ robocup/robocup.ino
@@ -73,10 +73,11 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.println("Display Started!");
+  display.drawBitmap(0, 0, Tritonbots_Logo, 128, 64, WHITE);
   display.display();
 
   delay(1000);
+  display.clearDisplay();
 
   pinMode(KICKER_PIN, OUTPUT);
   display.println("Kicker Pin Set!");
