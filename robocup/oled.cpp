4be4b5562792ea706be7fe98e33c1bacddb37bd2
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-06-25 17:22:04 -0700

    feat: add a simple OLED draw example to the setup code add oled.h/cpp for displayer functions and helpers note: code works on an esp32 with 3.3v on VCC and SCL is D22 and SDA is D21.

---
+++ robocup/oled.cpp
@@ -0,0 +1,66 @@
+/**
+ * @file TODO: write file header
+ */
+
+#include "oled.h"
+
+void testdrawline(Adafruit_SSD1306& display) {
+  int16_t i;
+
+  display.clearDisplay(); // Clear display buffer
+
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, SSD1306_WHITE);
+    display.display(); // Update screen with each newly-drawn line
+    delay(1);
+  }
+  for(i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+
+  for(i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+
+  for(i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000); // Pause for 2 seconds
+}
--- robocup/robocup.ino
+++ robocup/robocup.ino
@@ -9,11 +9,13 @@
 
 #include "commands.h"
 #include "credentials.h"
+#include "oled.h"
 
 #define BAUD_RATE 115200
 
 static WiFiUDP udp;
 static ACAN2517FD can(MCP2517_CS, SPI, MCP2517_INT);
+static Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 static Moteus* Motors[NUM_MOTORS]{ nullptr };
 static Moteus* Wheels[NUM_WHEELS]{ nullptr };
@@ -55,6 +57,37 @@ void setup() {
   Serial.println("Start SPI!");
   SPI.begin();
   Serial.println("SPI Started!");
+
+  Wire.begin();
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C) /*&&
+      !display.begin(SSD1306_SWITCHCAPVCC, 0x3D)*/) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  // display.display() is NOT necessary after every single drawing command,
+  // unless that's what you want...rather, you can batch up a bunch of
+  // drawing operations and then update the screen all at once by calling
+  // display.display(). These examples demonstrate both approaches...
+
+  testdrawline(display);      // Draw many lines
 
   Serial.println("Start Settings!");
   // Run CAN-FD at 1 Mbit/s for both arbitration and data.
