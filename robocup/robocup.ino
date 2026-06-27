ec6be59717d4617fc7ceeb116c718ba8b9d69e39
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-06-27 13:10:22 -0700

    quick-fix: comment out "Setup display running..." print statement

--- robocup/robocup.ino
+++ robocup/robocup.ino
@@ -94,7 +94,7 @@ void setup() {
 
   // i2cScanner(Wire);
 
-  Serial.println("Setup display running...");
+  // Serial.println("Setup display running...");
 
   // // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   // if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C) /*&&
